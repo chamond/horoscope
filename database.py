3f1490474698faf1efca80413d6236700eb7bf3e
chamond <dsamokhvalov@profitbase.ru>
2024-09-05 17:36:23 +0500

    exclusions

--- database.py
+++ database.py
@@ -8,34 +8,8 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS vocabulary (id INTEGER PRIMARY KEY 
 def addExpression(type, value):
     cursor.execute('''INSERT INTO vocabulary (type, value) VALUES (?, ?)''', (type, value))
     connection.commit()
-
-# addExpression('adjective', 'Караульный')
-# addExpression('noun', 'Леха')
-# addExpression('verb', 'Накурится амурской химки')
-#
-# addExpression('adjective', 'Няшный')
-# addExpression('noun', 'Поросенок')
-# addExpression('verb', 'Наплюет тебе в табло')
-#
-# addExpression('adjective', 'Бородатый')
-# addExpression('noun', 'Таксист')
-# addExpression('verb', 'Станет твоим новым мужем')
-#
-# addExpression('adjective', 'Симпатичный')
-# addExpression('noun', 'Нотариус')
-# addExpression('verb', 'Вьебет все твои деньги')
-#
-# addExpression('adjective', 'Накаченный')
-# addExpression('noun', 'Чигма')
-# addExpression('verb', 'Сделает тебе больно')
-#
-# addExpression('adjective', 'Низкоуровневый')
-# addExpression('noun', 'Андрей Кревский')
-# addExpression('verb', 'Наденет тебе трусы на жопу')
-#
-# addExpression('adjective', 'Пушистенький')
-# addExpression('noun', 'Металлург')
-# addExpression('verb', 'Даст тебе в долг')
+    cursor.execute('''DELETE FROM vocabulary WHERE rowid NOT IN (SELECT MIN(rowid) FROM vocabulary GROUP BY type, value)''')
+    connection.commit()
 
 cursor.execute('''DELETE FROM vocabulary WHERE rowid NOT IN (SELECT MIN(rowid) FROM vocabulary GROUP BY type, value)''')
 
